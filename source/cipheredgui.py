719d5eb7c5e417fc935af130baab290e1b53143e
ghelioth~ <heliothndoko@gmail.com>
2023-03-24 19:54:48 +0100

    Edit_Chiffrement

--- source/cipheredgui.py
+++ source/cipheredgui.py
@@ -116,3 +116,74 @@ class CipheredGUI (BasicGUI):
         super().send_message(message_chiffre)
 
 
+    def _create_chat_window(self)->None:
+        # chat windows
+        # known bug : the add_input_text do not display message in a user friendly way
+        with dpg.window(label="Chat", pos=(0, 0), width=800, height=600, show=False, tag="chat_windows", on_close=self.on_close):
+            dpg.add_input_text(default_value="Readonly\n\n\n\n\n\n\n\nfff", multiline=True, readonly=True, tag="screen", width=790, height=525)
+            dpg.add_input_text(default_value="Dites bonjour ...", tag="input", on_enter=True, callback=self.text_callback, width=790)
+
+
+    def _create_menu(self)->None:
+        # menu (file->connect)
+        with dpg.viewport_menu_bar():
+            with dpg.menu(label="File"):
+                dpg.add_menu_item(label="Connect", callback=self.connect)
+
+
+    def create(self):
+        # create the context and all windows
+        dpg.create_context()
+
+        self._create_chat_window()
+        self._create_connection_window()
+        self._create_menu()        
+            
+        dpg.create_viewport(title='Secure chat - or not', width=800, height=600)
+        dpg.setup_dearpygui()
+        dpg.show_viewport()
+
+
+    def update_text_screen(self, new_text:str)->None:
+        # from a nex_text, add a line to the dedicated screen text widget
+        text_screen = dpg.get_value("screen")
+        text_screen = text_screen + "\n" + new_text
+        dpg.set_value("screen", text_screen)
+
+
+    def text_callback(self, sender, app_data)->None:
+        # every time a enter is pressed, the message is gattered from the input line
+        text = dpg.get_value("input")
+        self.update_text_screen(f"Me: {text}")
+        self.send(text)
+        dpg.set_value("input", "")
+
+
+    def connect(self, sender, app_data)->None:
+        # callback used by the menu to display connection windows
+        dpg.show_item("connection_windows")
+
+    def loop(self):
+        # main loop
+        while dpg.is_dearpygui_running():
+            self.recv()
+            dpg.render_dearpygui_frame()
+
+        dpg.destroy_context()
+
+    def on_close(self):
+        # called when the chat windows is closed
+        self._client.stop()
+        self._client = None
+        self._callback = None
+
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    # instanciate the class, create context and related stuff, run the main loop
+    client = CipheredGUI()
+    client.create()
+    client.loop()
